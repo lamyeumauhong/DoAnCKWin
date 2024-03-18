b37a61d9d45c3e27eac635eb84a5a97452aa03fd
lamyeumauhong <duongvu874@gmail.com>
2024-03-18 15:43:25 +0700

    upload

---
+++ DoAnCKWin/DTO/NhaCungCapDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCKWin.DTO
+{
+    internal class NhaCungCapDTO
+    {
+    }
+}
--- DoAnCKWin/Properties/Resources.Designer.cs
+++ DoAnCKWin/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace DoAnCKWin.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap gio_hang {
+            get {
+                object obj = ResourceManager.GetObject("gio_hang", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hinh_shop_login {
             get {
                 object obj = ResourceManager.GetObject("hinh_shop_login", resourceCulture);
